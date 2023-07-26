193e47e2e0003adca8276320c8e7c3b7b14b8b07
phucanscloud <90325587+phucanscloud@users.noreply.github.com>
2023-07-26 10:33:59 +0700

    WIP: Added activity cans call xml

--- app/src/main/java/cc/cans/canscloud/demoappinsdk/MainActivity.kt
+++ app/src/main/java/cc/cans/canscloud/demoappinsdk/MainActivity.kt
@@ -1,10 +1,12 @@
 package cc.cans.canscloud.demoappinsdk
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import cc.cans.canscloud.sdk.Cans
 import cc.cans.canscloud.demoappinsdk.databinding.ActivityMainBinding
+import cc.cans.canscloud.sdk.call.CansCallActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +24,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonCall.setOnClickListener {
-            Cans.startCall("0957414609")
+//            Cans.startCall("0957414609")
+
+            val intent = Intent(this, CansCallActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- canscloud-android-sdk/src/main/java/cc/cans/canscloud/sdk/call/CansCallActivity.kt
+++ canscloud-android-sdk/src/main/java/cc/cans/canscloud/sdk/call/CansCallActivity.kt
@@ -2,9 +2,17 @@ package cc.cans.canscloud.sdk.call
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import cc.cans.canscloud.sdk.databinding.ActivityCansCallBinding
 
 class CansCallActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityCansCallBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityCansCallBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
