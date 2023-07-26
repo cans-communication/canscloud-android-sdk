b6e2067ddfd4d5c65c5872b3403714eef4efe3f9
phucanscloud <90325587+phucanscloud@users.noreply.github.com>
2023-07-26 10:24:42 +0700

    WIP: Added Cans Call Activity

---
+++ canscloud-android-sdk/src/main/java/cc/cans/canscloud/sdk/call/CansCallActivity.kt
@@ -0,0 +1,10 @@
+package cc.cans.canscloud.sdk.call
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class CansCallActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
